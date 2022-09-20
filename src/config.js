743b2d9db239b86ed5f4e4922e599e4e5981a5fc
Max_S <maxi.sm117@gmail.com>
2022-09-20 02:44:39 -0300

    :zap: command start added

--- src/config.js
+++ src/config.js
@@ -1,7 +1,6 @@
-export const PORT = process.env.PORT || 3000
-
-export const DB_HOST = process.env.DB_HOST || 'localhost'
-export const DB_USER = process.env.DB_USER || 'root'
-export const DB_PASSWORD = process.env.DB_PASSWORD || '1777'
-export const DB_NAME = process.env.DB_NAME || 'usersdb'
-export const DB_PORT = process.env.DB_PORT || 3306
+export const PORT = process.env.PORT
+export const DB_HOST = process.env.DB_HOST
+export const DB_USER = process.env.DB_USER
+export const DB_PASSWORD = process.env.DB_PASSWORD
+export const DB_NAME = process.env.DB_NAME
+export const DB_PORT = process.env.DB_PORT
